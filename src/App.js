23c2a84f2c5a42e1b16ab9515a2bd171a2f3970f
Zeerex23 <tobilobadiekola@gmail.com>
2022-11-04 10:18:42 +0100

    refactor: added React Router Dom to make app implement the routing feature

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route
+} from 'react-router-dom';
+
 import Header from './components/Header';
 import Links from './components/Links';
+import Contact from './components/Contact';
 import Footer from './components/Footer';
 
 import './styles.css';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Links />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />
+  }
+])
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Links />
+      <RouterProvider router={router} />
       <Footer />
     </div>
   );
